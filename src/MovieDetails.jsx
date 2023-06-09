a09cfe320c4718fd0f1ca429e11e4026cc9cdf4d
waynesdotdev <fox.wayne1@gmail.com>
2023-06-09 13:29:02 +0100

    clean up function done

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -55,11 +55,17 @@ export default function MovieDetails({
   }, [title])
 
   useEffect(() => {
-    document.addEventListener('keydown', function (e) {
+    function callback(e) {
       if (e.code === 'Escape') {
         onCloseMovie()
       }
-    })
+    }
+
+    document.addEventListener('keydown', callback)
+
+    return function () {
+      document.removeEventListener('keydown', callback)
+    }
   }, [onCloseMovie])
 
   function handleAdd() {
