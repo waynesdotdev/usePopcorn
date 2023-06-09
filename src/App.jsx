5787335b80fcca555c9d0ee277ecf6052fdfee76
waynesdotdev <fox.wayne1@gmail.com>
2023-06-09 13:10:37 +0100

    controller added fro http requests

--- src/App.jsx
+++ src/App.jsx
@@ -84,12 +84,15 @@ export default function App() {
   }
 
   useEffect(() => {
+    const controller = new AbortController()
+
     async function fetchMovies() {
       try {
         setIsLoading(true)
         setError('')
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${key}&s=${query}`
+          `http://www.omdbapi.com/?apikey=${key}&s=${query}`,
+          { signal: controller.signal }
         )
 
         if (!res.ok)
@@ -100,9 +103,10 @@ export default function App() {
         if (data.Response === 'False') throw new Error('Movie Not Found')
 
         setMovies(data.Search)
+        setError('')
       } catch (err) {
         console.error(err.message)
-        setError(err.message)
+        if (err.name !== 'AbortError') setError(err.message)
       } finally {
         setIsLoading(false)
       }
@@ -115,6 +119,10 @@ export default function App() {
     }
 
     fetchMovies()
+
+    return function () {
+      controller.abort()
+    }
   }, [query])
 
   return (
